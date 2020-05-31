ebe9cd728bd86a206c2dccecf2156ad5bdcf6cf8
maisam216 <maisam_alatrach@hotmail.com>
2020-05-31 15:03:08 +1000

    Update About.java

--- src/com/pages/About.java
+++ src/com/pages/About.java
@@ -1,4 +1,10 @@
-    package com.pages;
+/*
+ * Maisam Alatrach 20091817
+ * Oudai Fayek 20091861
+ * Latrobe University
+ * 29/05/2020
+ */
+package com.pages;
 
 import java.io.IOException;
 import javax.servlet.RequestDispatcher;
